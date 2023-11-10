df5f2f3e5b5181d6e3a8a88e6225da6b5de83d6b
Krzysztof Sedek <krzysztof.sedek@prudential.pl>
2023-11-10 14:48:42 +0100

    master: poprawka

---
+++ src/main/java/pl/aswit/leetcode/task2190/Test2190.java
@@ -0,0 +1,15 @@
+package pl.aswit.leetcode.task2190;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test2190 {
+    public static void main(String [] args){
+        var solution = new Solution();
+//        assertThat(solution.mostFrequent(new int[]{1, 100, 200, 1, 100}, 1)).isEqualTo(100);
+//        assertThat(solution.mostFrequent(new int[]{2,2,2,2,3}, 2)).isEqualTo(2);
+        assertThat(solution.mostFrequent(new int[]{2,1000,2,1000,2,3}, 2)).isEqualTo(2);
+//        assertThat(solution.mostFrequent(new int[]{1, 1}, 1)).isEqualTo(2);
+    }
+
+}
