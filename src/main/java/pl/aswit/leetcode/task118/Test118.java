b745af73e5cb975f90f23632de712b7bb2a5669b
Krzysztof Sedek <krzysztof.sedek@prudential.pl>
2023-11-12 18:05:56 +0100

    master: Merge branch 'master' of https://github.com/ksedek89/leetcode # Conflicts: # src/main/java/pl/aswit/leetcode/task1317/Solution.java

---
+++ src/main/java/pl/aswit/leetcode/task118/Test118.java
@@ -0,0 +1,12 @@
+package pl.aswit.leetcode.task118;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test118 {
+
+    public static void main(String[] args) {
+        var solution = new Solution();
+        System.out.println(solution.generate(5));
+    }
+}
