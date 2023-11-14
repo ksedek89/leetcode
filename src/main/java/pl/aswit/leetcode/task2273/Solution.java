25636260adfdfd6cd77864efc1b97dc7dc9a1763
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2023-11-14 13:19:23 +0100

    task 2273

---
+++ src/main/java/pl/aswit/leetcode/task2273/Solution.java
@@ -0,0 +1,29 @@
+package pl.aswit.leetcode.task2273;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    //beats 62%
+    public List<String> removeAnagrams(String[] words) {
+        List<String> list = new ArrayList<>();
+        List<String> listSorted = new ArrayList<>();
+        for(String word: words){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            listSorted.add(new String(chars));
+        }
+
+        list.add(words[0]);
+        for(int i = 1; i < words.length; i++){
+            if(!listSorted.get(i).equals(listSorted.get(i-1))){
+                list.add(words[i]);
+            }
+        }
+        return list;
+    }
+
+
+}
---
+++ src/main/java/pl/aswit/leetcode/task2273/Test.java
@@ -0,0 +1,15 @@
+package pl.aswit.leetcode.task2273;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test {
+    public static void main(String [] args){
+        var solution = new Solution();
+        assertThat(solution.removeAnagrams(new String[]{"abba","baba","bbaa","cd","cd"})).isEqualTo(List.of("abba","cd"));
+        assertThat(solution.removeAnagrams(new String[]{"a","b","a"})).isEqualTo(List.of("a","b", "a"));
+    }
+
+}
