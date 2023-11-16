232618c82051c71eb2373825c6e8458043cf22cc
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2023-11-16 11:46:21 +0100

    task 2500

---
+++ src/main/java/pl/aswit/leetcode/task2500/Solution.java
@@ -0,0 +1,45 @@
+package pl.aswit.leetcode.task2500;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    //beats 99%
+    public int deleteGreatestValue(int[][] grid) {
+        int sum = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int[] arr : grid) {
+            Arrays.sort(arr);
+        }
+        for(int i = m - 1 ;i>=0;i--) {
+            int max = 0;
+            for(int j = 0; j<n; j++){
+                int currentValue = grid[j][i];
+                if(currentValue > max){
+                    max = currentValue;
+                }
+            }
+            sum += max;
+        }
+        return sum;
+    }
+    //beats 32%
+    public int deleteGreatestValueWeak(int[][] grid) {
+        int sum = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int[] arr : grid) {
+            Arrays.sort(arr);
+        }
+        for(int i = m - 1 ;i>=0;i--) {
+            int[] currentColumn = new int[n];
+            for(int j = 0; j<n; j++){
+                currentColumn[j] = grid[j][i];
+            }
+            sum += Arrays.stream(currentColumn).max().getAsInt();
+        }
+        return sum;
+    }
+
+}
---
+++ src/main/java/pl/aswit/leetcode/task2500/Test.java
@@ -0,0 +1,12 @@
+package pl.aswit.leetcode.task2500;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test {
+    public static void main(String [] args){
+        var solution = new Solution();
+        assertThat(solution.deleteGreatestValue(new int[][]{{1,2,4},{3,3,1}})).isEqualTo(8);
+    }
+
+}
