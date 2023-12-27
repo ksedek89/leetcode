739f5d895d72d8212c57d4368f28564e8d80139a
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2023-12-27 14:29:35 +0100

    task 8

--- src/main/java/pl/aswit/leetcode/medium/task8/Solution.java
+++ src/main/java/pl/aswit/leetcode/medium/task8/Solution.java
@@ -8,10 +8,12 @@ public class Solution {
 
         char[] charArray = s.toCharArray();
         boolean signExists = false;
+        boolean hasDigit = false;
         for (int i = 0; i < charArray.length; i++) {
             char currentChar = charArray[i];
             if((currentChar >= '0' && currentChar <= '9') ){
                 sb.append(currentChar);
+                hasDigit = true;
                 long currentLong = Long.valueOf(sb.toString());
                 if (currentLong < Integer.MIN_VALUE) {
                     return Integer.MIN_VALUE;
@@ -19,48 +21,21 @@ public class Solution {
                 if (currentLong > Integer.MAX_VALUE) {
                     return Integer.MAX_VALUE;
                 }
-            }else if(i == 0){
-                if(currentChar == '+' || currentChar == '-'){
-                    if(!signExists){
-                        sb.append(currentChar);
-                        signExists = true;
-                    }else{
-                        return 0;
-                    }
-                }else{
-                    return 0;
-                }
             }else if(currentChar == '+' || currentChar == '-'){
-                if(!signExists){
-                    if(i == 0){
-                        sb.append(currentChar);
-                        signExists = true;
-                    }
-
+                if(!signExists && i == 0){
+                    sb.append(currentChar);
+                    signExists = true;
                 }else{
                     break;
                 }
-                break;
             }else{
                 break;
             }
         }
-        if(sb.toString().length() == 0){
+        if(sb.toString().length() == 0 || !hasDigit){
             return 0;
         }
-
-        try {
-            long currentLong = Long.valueOf(sb.toString());
-            if (currentLong < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
-            }
-            if (currentLong > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            }
-            return Integer.valueOf(sb.toString());
-        }catch (Exception e){
-            return 0;
-        }
+        return Integer.valueOf(sb.toString());
     }
 }
 
