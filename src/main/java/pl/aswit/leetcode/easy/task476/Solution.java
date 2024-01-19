c9bab82caf7ef7d0a44237004ded03cdaf413ccf
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2024-01-19 15:09:41 +0100

    task 476

---
+++ src/main/java/pl/aswit/leetcode/easy/task476/Solution.java
@@ -0,0 +1,39 @@
+package pl.aswit.leetcode.easy.task476;
+
+
+import pl.aswit.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+/*    public int findComplement(int num) {
+        String binaryString = Integer.toBinaryString(num);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < binaryString.length() ; i++) {
+            if(binaryString.charAt(i) == '1'){
+                sb.append('0');
+            }else{
+                sb.append('1');
+            }
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }*/
+
+    public int findComplement(int num) {
+        int mask = 0;
+        int n = num;
+
+        // Build a mask with 1s in positions corresponding to the binary digits of num
+        while (n > 0) {
+            n /= 2;
+            mask = (mask << 1) + 1;
+        }
+
+        // XOR the num with the mask to get the complement
+        return num ^ mask;
+    }
+}
---
+++ src/main/java/pl/aswit/leetcode/easy/task476/Test.java
@@ -0,0 +1,20 @@
+package pl.aswit.leetcode.easy.task476;
+
+
+import pl.aswit.leetcode.utils.TreeNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test {
+    public static void main(String [] args){
+
+        Solution solution = new Solution();
+
+
+
+//        assertThat(solution.findComplement(5)).isEqualTo(2);
+        assertThat(solution.findComplement(4389)).isEqualTo(3);
+
+    }
+
+}
