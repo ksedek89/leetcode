5ac72d9d4575878c36813c34267ffc6ca031ac0c
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2024-01-20 09:05:01 +0100

    task 27

---
+++ src/main/java/pl/aswit/leetcode/easy/task27/Solution.java
@@ -0,0 +1,46 @@
+package pl.aswit.leetcode.easy.task27;
+
+import pl.aswit.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//https://www.programiz.com/dsa/binary-tree
+//https://www.javatpoint.com/inorder-traversal
+
+public class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curElement = root;
+        while (curElement != null || stack.size() != 0){
+            while (curElement != null){
+                stack.push(curElement);
+                curElement = curElement.left;
+            }
+            TreeNode pop = stack.pop();
+            curElement = pop.right;
+            list.add(pop.val);
+        }
+        return list;
+    }
+
+    public List<Integer> inorderTraversalRecursive(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        addToList(list, root);
+        return list;
+    }
+
+    private void addToList(List<Integer> list, TreeNode node){
+        if(node == null){
+            return;
+        }
+        addToList(list, node.left);
+        list.add(node.val);
+        addToList(list, node.right);
+    }
+}
+
