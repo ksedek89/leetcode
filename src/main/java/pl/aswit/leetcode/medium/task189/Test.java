2aa3e12e4ee8c7ce433cedf4b19b78af841ad2d1
Krzysztof Sędek <krzysztof.sedek@prudential.pl>
2024-01-20 09:41:44 +0100

    task 169,189

---
+++ src/main/java/pl/aswit/leetcode/medium/task189/Test.java
@@ -0,0 +1,18 @@
+package pl.aswit.leetcode.medium.task189;
+
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test {
+    public static void main(String [] args) throws IOException {
+        var solution = new Solution();
+//        solution.rotate(new int[]{1,2,3,4,5,6,7}, 3);
+//        solution.rotate(new int[]{1,2,3,4,5,6,7}, 3);
+        solution.rotate(new int[]{1,2}, 3);
+
+
+    }
+
+}
