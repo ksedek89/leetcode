8d263533a017e8626a04d81933d98a0799bb239e
Krzysztof Sedek <krzysztof.sedek@prudential.pl>
2024-02-04 12:05:17 +0100

    master: task 1027

---
+++ src/main/java/pl/aswit/leetcode/medium/task1027/Test.java
@@ -0,0 +1,18 @@
+package pl.aswit.leetcode.medium.task1027;
+
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test {
+    public static void main(String [] args) throws IOException {
+        var solution = new Solution();
+
+//        assertThat(solution.longestArithSeqLength(new int[]{3,6,9,12})).isEqualTo(4);
+//        assertThat(solution.longestArithSeqLength(new int[]{9,4,7,2,10})).isEqualTo(3);
+//        assertThat(solution.longestArithSeqLength(new int[]{83,20,17,43,52,78,68,45})).isEqualTo(2);
+
+    }
+
+}
